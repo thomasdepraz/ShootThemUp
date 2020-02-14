f49b38f6a2821675f45b4c37eb6b12e850a4ef42
Thomas Depraz <t.deprazdepland@rubika-edu.com>
2020-02-14 16:13:28 +0100

    Camera setup //Stable

---
+++ Assets/ScriptsThomas/PlayerMovementKeyboard.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementKeyboard : MonoBehaviour
+{
+    [Header("Logic")]
+    private float horizontalMovement;
+    private float verticalMovement;
+    private Vector2 direction;
+    public Rigidbody2D rb;
+
+    [Header("Tweaks")]
+    [Range(0f, 5f)]
+    public float speedModifier = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(horizontalMovement != 0 && verticalMovement !=0)
+        {
+            //il se passe des trucs
+        }
+        rb.velocity = direction * Time.deltaTime * speedModifier;
+
+    }
+
+    private void GetInput()
+    {
+        horizontalMovement = Input.GetAxis("Horizontal");
+        verticalMovement = Input.GetAxis("Vertical");
+        direction = new Vector2(horizontalMovement, verticalMovement).normalized;
+    }
+}
