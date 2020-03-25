a92d726f10037b970dd97edc9746ec210b60d389
Thomas Depraz <t.deprazdepland@rubika-edu.com>
2020-03-25 12:08:16 +0100

    Merge to main scene

---
+++ Assets/ScriptsThomas/Lives.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Lives : MonoBehaviour
+{
+    public int lives = 3;
+    public string textToDisplay;
+    public Text text;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lives > 99)
+            textToDisplay = "Lives : +99";
+
+        else
+            textToDisplay ="Lives : " + lives.ToString();
+
+        text.text = textToDisplay;
+
+        if (lives >= 0)
+            GameOver();
+    }
+
+    public void LifeUp()
+    {
+        lives++;
+    }
+
+    public void LifeDown()
+    {
+        lives--;
+    }
+
+    public void GameOver()
+    {
+        //le game over wlh
+    }
+}
