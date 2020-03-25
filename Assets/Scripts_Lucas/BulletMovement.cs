ccc8a4da6addf543d638c858501bae4bacdb0032
lucas <l.deutschmann@rubika-edu.com>
2020-03-25 11:15:42 +0100

    Shooting Works

---
+++ Assets/Scripts_Lucas/BulletMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMovement : MonoBehaviour
+{
+    public float bulletSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(0, 1, 0) * Time.deltaTime * bulletSpeed;
+    }
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts_Lucas/Shooting.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public float cooldown;
+    public GameObject bullet;
+    bool canshoot = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space) && canshoot == true)
+        {
+            StartCoroutine(ShootCooldown());
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject Bullet = Instantiate(bullet, gameObject.transform.position, Quaternion.identity);
+    }
+    IEnumerator ShootCooldown()
+    {
+        canshoot = false;
+        yield return new WaitForSeconds(cooldown);
+        canshoot = true;
+    }
+}
